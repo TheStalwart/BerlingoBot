3766dd5215f255c35a3b026a322506da77308b96
Pavel Shevchuk <stlwrt@gmail.com>
2024-09-02 16:19:09 +0000

    Better /v command output - will now generate a link to GitHub that resolves to pretty embed in Discord

--- commands/diagnostics/v.js
+++ commands/diagnostics/v.js
@@ -7,7 +7,15 @@ module.exports = {
 		.setName('v')
 		.setDescription('Bot version'),
 	async execute(interaction) {
-        const lastCommitSummary = await simpleGit().show('--summary')
-		await interaction.reply(`${lastCommitSummary}`);
+        const lastCommitHash = await simpleGit().show(['--format=%H', '--summary'])
+		simpleGit().listRemote(['--get-url'], (err, data) => {
+			if (!err) {
+				console.log(data);
+				interaction.reply(`${data.trim()}/commit/${lastCommitHash}`);
+			} else {
+				console.error(err)
+				interaction.reply(`${lastCommitHash}`);
+			}
+		 });
 	},
 };
