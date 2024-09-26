54c59b74f9170d22978cb3e75b403620adbe0f8b
Pavel Shevchuk <stlwrt@gmail.com>
2024-09-26 19:41:09 +0300

    Set javascript formatter to Prettier, reformatted neofetch.js

--- commands/system/neofetch.js
+++ commands/system/neofetch.js
@@ -1,26 +1,26 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { exec } = require('child_process');
+const { SlashCommandBuilder } = require("discord.js");
+const { exec } = require("child_process");
 
 module.exports = {
-	cooldown: 5,
-	data: new SlashCommandBuilder()
-		.setName('neofetch')
-		.setDescription('Run neofetch'),
-	async execute(interaction) {
-        await interaction.deferReply();
-        exec('neofetch --stdout', async (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error: ${error.message}`);
-            await interaction.editReply(`Error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.error(`Stderr: ${stderr}`);
-            await interaction.editReply(`Stderr: ${stderr}`);
-            return;
-        }
-        console.log(`Stdout: ${stdout}`);
-        await interaction.editReply(`\`\`\`\n${stdout}\`\`\``);
-        });
-	},
+  cooldown: 5,
+  data: new SlashCommandBuilder()
+    .setName("neofetch")
+    .setDescription("Run neofetch"),
+  async execute(interaction) {
+    await interaction.deferReply();
+    exec("neofetch --stdout", async (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        await interaction.editReply(`Error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`Stderr: ${stderr}`);
+        await interaction.editReply(`Stderr: ${stderr}`);
+        return;
+      }
+      console.log(`Stdout: ${stdout}`);
+      await interaction.editReply(`\`\`\`\n${stdout}\`\`\``);
+    });
+  },
 };
