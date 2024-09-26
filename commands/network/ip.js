88bb73b56cfa9e0f044bfea74f02d335ed4922d2
Pavel Shevchuk <stlwrt@gmail.com>
2024-09-26 20:17:22 +0300

    Reformatted ip.js with Prettier

--- commands/network/ip.js
+++ commands/network/ip.js
@@ -1,23 +1,25 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-	cooldown: 5,
-	data: new SlashCommandBuilder()
-		.setName('ip')
-		.setDescription('Public IP address'),
-	async execute(interaction) {
-        fetch(new Request("https://ifconfig.me", {
-            headers: new Headers({
-                "User-Agent": "curl/8.7.1"
-            })
-        }))
-            .then((response) => response.text())
-            .then((text) => {
-                interaction.reply(`Public IP: ${text}`);
-            })
-            .catch((error) => {
-                console.error(error.message);
-                interaction.reply(`Error: ${error.message}`);
-            });
-	},
+  cooldown: 5,
+  data: new SlashCommandBuilder()
+    .setName("ip")
+    .setDescription("Public IP address"),
+  async execute(interaction) {
+    fetch(
+      new Request("https://ifconfig.me", {
+        headers: new Headers({
+          "User-Agent": "curl/8.7.1",
+        }),
+      })
+    )
+      .then((response) => response.text())
+      .then((text) => {
+        interaction.reply(`Public IP: ${text}`);
+      })
+      .catch((error) => {
+        console.error(error.message);
+        interaction.reply(`Error: ${error.message}`);
+      });
+  },
 };
