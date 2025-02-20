265eb772fdadad793123a04f3e4fbc0027d89eb0
Pavel Shevchuk <stlwrt@gmail.com>
2025-02-20 06:40:34 +0200

    Introduced speedtest command

---
+++ commands/network/speedtest.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { exec } = require("child_process");
+
+module.exports = {
+  cooldown: 5,
+  data: new SlashCommandBuilder()
+    .setName("speedtest")
+    .setDescription("Run speedtest-cli"),
+  async execute(interaction) {
+    await interaction.deferReply();
+    exec("speedtest-cli --simple --share", (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        interaction.editReply(`Error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`Stderr: ${stderr}`);
+        interaction.editReply(`Stderr: ${stderr}`);
+        return;
+      }
+      console.log(`Stdout: ${stdout}`);
+      interaction.editReply(`\`\`\`\n${stdout}\`\`\``);
+    });
+  },
+};
